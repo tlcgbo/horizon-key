41f33ca3d2d247d97f2891973c680b83439278f2
unknown <iolufiranye@gmail.com>
2025-07-25 19:14:38 +0100

    third commit

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwrxpMDHbRSuOpdapdXdvabIUj7ysIhOI",
+  authDomain: "horizon-key-9f054.firebaseapp.com",
+  projectId: "horizon-key-9f054",
+  storageBucket: "horizon-key-9f054.firebasestorage.app",
+  messagingSenderId: "675446115778",
+  appId: "1:675446115778:web:23cdb1134f59af98ad9a5b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
